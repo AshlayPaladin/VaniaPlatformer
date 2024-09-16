491fecb45d3071370d214cb19660bed255507e73
Ashlay <141889414+AshlayPaladin@users.noreply.github.com>
2024-09-16 08:49:08 -0500

    Cleaned up USING statements on all classes

--- EntityComponentSystem/Components/AnimationComponent.cs
+++ EntityComponentSystem/Components/AnimationComponent.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace VaniaPlatformer.ECS;
 
--- EntityComponentSystem/Components/InputComponent.cs
+++ EntityComponentSystem/Components/InputComponent.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Microsoft.Xna.Framework.Input;
 
 namespace VaniaPlatformer.ECS;
